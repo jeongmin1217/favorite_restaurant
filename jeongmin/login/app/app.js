7c9b0faa554eb5c2bfb7c7bc46cde13ec2c9d70e
Jeongmin <balljm@naver.com>
2022-06-01 00:14:17 +0900

    Modify login code

---
+++ jeongmin/login/app/app.js
@@ -0,0 +1,14 @@
+"use strict";
+//모듈
+const express = require("express");
+const app = express();
+//라우팅
+const home = require("./src/routes/home");
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
+
+app.use("/", home); //미들웨어 등록해주는 method
+
+module.exports = app;
